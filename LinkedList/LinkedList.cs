e09c4b0cb65604b897315c5174bb8dbc32d68717
10934117 <10934117@INFO1-03>
2024-10-08 12:01:42 +0200

    Aggiungere i file di progetto.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T>
+    {
+        public int Count { get; set; }
+        public int First { get; set; }
+        public int Last { get; set; }
+
+        LinkedList()
+        {
+            Count = 0;
+            First = 0;
+            Last = 0;
+        }
+
+        public void AddAfter()
+        {
+
+        }
+    }
+}
