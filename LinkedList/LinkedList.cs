f648e2cda788246f258538a59fde344cd06e37c7
akira_kage <alok95ita@gmail.com>
2024-10-09 17:50:39 +0200

    created new add functions

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -12,26 +12,74 @@ namespace LinkedList
 
         public LinkedList(){}
 
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            if(Head == null) Last = node;
+            node.Next = Head;
+            Head = node;
+            Count++;
+        }
+
         public void AddFirst(T value)
         {
             LinkedListNode<T> element = new LinkedListNode<T>(value) { Next = Head };
-            if(Head == null) Last = element;
-            Head = element;
+            AddFirst(element);
+        }
+
+        public void AddLast(LinkedListNode<T> node)
+        {
+            if (Last == null)
+            {
+                AddFirst(node);
+                return;
+            }
+            Last.Next = node;
+            Last = node;
             Count++;
         }
 
         public void AddLast(T value)
         {
-            LinkedListNode<T> element = new LinkedListNode<T>(value) { Next = null };
-            if(Last != null) Last.Next = element;
-            else Head = element;
-            Last = element;
+            LinkedListNode<T> element = new LinkedListNode<T>(value);
+            AddLast(element);
+        }
+
+
+        public void AddAfter(LinkedListNode<T> targetNode, LinkedListNode<T> newNode)
+        {
+            newNode.Next = targetNode.Next;
+            targetNode.Next = newNode;
+            if (targetNode == Last) Last = newNode;
             Count++;
         }
 
-        public void AddAfter()
+        public void AddAfter(LinkedListNode<T> targetNode, T value)
         {
+            LinkedListNode<T> element = new LinkedListNode<T>(value) { Next = targetNode.Next };
+            AddAfter(targetNode, element);
+        }
 
+        public void AddBefore(LinkedListNode<T> targetNode, LinkedListNode<T> newNode)
+        {
+            if(Head == null || targetNode == Head)
+            {
+                AddFirst(newNode);
+                return;
+            }
+            LinkedListNode<T> currentNode = Head.Next;
+            while(currentNode.Next != targetNode && currentNode.Next != null) currentNode = currentNode.Next;
+            if(currentNode.Next == null)
+            {
+                Console.WriteLine("Nodo non trovato");
+                return;
+            }
+            AddAfter(currentNode, newNode);
+        }
+
+        public void AddBefore(LinkedListNode<T> targetNode, T value)
+        {
+            LinkedListNode<T> element = new LinkedListNode<T>(value);
+            AddBefore(targetNode, element);
         }
     }
 
