bdb6fb5ada044f302632c7d87ca85b6061bae4dd
akira_kage <alok95ita@gmail.com>
2024-10-15 00:27:39 +0200

    slight changes

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -1,191 +1,194 @@
 ﻿using System;
 
-public class LinkedListNode<T>
+namespace LinkedList
 {
-    public T Value { get; set; }
-    public LinkedListNode<T> Next { get; set; }
-    public LinkedListNode<T> Previous { get; set; }
-
-    public LinkedListNode(T value)
+    public class LinkedListNode<T>
     {
-        Value = value;
-        Next = null;
-        Previous = null;
-    }
-}
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+        public LinkedListNode<T> Previous { get; set; }
 
-public class LinkedList<T>
-{
-    private LinkedListNode<T> Head;
-    private LinkedListNode<T> Last;
-    public int Count { get; private set; }
+        public LinkedListNode(T value)
+        {
+            Value = value;
+            Next = null;
+            Previous = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        private LinkedListNode<T> _head;
+        private LinkedListNode<T> _last;
+        public int Count { get; private set; }
     
-    public void AddFirst(LinkedListNode<T> node)
-    {
-        if (Head == null)
+        public void AddFirst(LinkedListNode<T> node)
         {
-            Head = Last = node;
-        }
-        else
-        {
-            node.Next = Head;
-            Head.Previous = node;
-            Head = node;
-        }
-        Count++;
-    }
-
-    public void AddFirst(T value) => AddFirst(new LinkedListNode<T>(value));
-
-    public void AddLast(LinkedListNode<T> node)
-    {
-        if (Last == null)
-        {
-            Head = Last = node;
-        }
-        else
-        {
-            node.Previous = Last;
-            Last.Next = node;
-            Last = node;
-        }
-        Count++;
-    }
-
-    public void AddLast(T value) => AddLast(new LinkedListNode<T>(value));
-
-    public void AddAfter(LinkedListNode<T> existingNode, LinkedListNode<T> newNode)
-    {
-        if (existingNode == null || newNode == null)
-        {
-            Console.WriteLine("Presenti nodi non esistenti nel comando, ricontrollare");
-            return;
+            if (_head == null)
+            {
+                _head = _last = node;
+            }
+            else
+            {
+                node.Next = _head;
+                _head.Previous = node;
+                _head = node;
+            }
+            Count++;
         }
 
-        newNode.Next = existingNode.Next;
-        newNode.Previous = existingNode;
-        if (existingNode.Next != null) existingNode.Next.Previous = newNode;
-        existingNode.Next = newNode;
+        public void AddFirst(T value) => AddFirst(new LinkedListNode<T>(value));
 
-        if (existingNode == Last) Last = newNode;
-        Count++;
-    }
-
-    public void AddAfter(LinkedListNode<T> existingNode, T value) 
-        => AddAfter(existingNode, new LinkedListNode<T>(value));
-
-    public void AddBefore(LinkedListNode<T> existingNode, LinkedListNode<T> newNode)
-    {
-        if (existingNode == null || newNode == null)
+        public void AddLast(LinkedListNode<T> node)
         {
-            Console.WriteLine("Presenti nodi non esistenti nel comando, ricontrollare");
-            return;
+            if (_last == null)
+            {
+                _head = _last = node;
+            }
+            else
+            {
+                node.Previous = _last;
+                _last.Next = node;
+                _last = node;
+            }
+            Count++;
         }
 
-        newNode.Previous = existingNode.Previous;
-        newNode.Next = existingNode;
-        if (existingNode.Previous != null) existingNode.Previous.Next = newNode;
-        existingNode.Previous = newNode;
+        public void AddLast(T value) => AddLast(new LinkedListNode<T>(value));
 
-        if (existingNode == Head) Head = newNode;
-        Count++;
-    }
-
-    public void AddBefore(LinkedListNode<T> existingNode, T value) 
-        => AddBefore(existingNode, new LinkedListNode<T>(value));
-
-    public bool Contains(T value)
-    {
-        return Find(value) != null;
-    }
-
-    public LinkedListNode<T> Find(T value)
-    {
-        LinkedListNode<T> current = Head;
-        while (current != null)
+        public void AddAfter(LinkedListNode<T> existingNode, LinkedListNode<T> newNode)
         {
-            if (current.Value.Equals(value))
-                return current;
-            current = current.Next;
-        }
-        return null;
-    }
+            if (existingNode == null || newNode == null)
+            {
+                Console.WriteLine("Presenti nodi non esistenti nel comando, ricontrollare");
+                return;
+            }
 
-    public LinkedListNode<T> FindLast(T value)
-    {
-        LinkedListNode<T> current = Last;
-        while (current != null)
-        {
-            if (current.Value.Equals(value))
-                return current;
-            current = current.Previous;
-        }
-        return null;
-    }
+            newNode.Next = existingNode.Next;
+            newNode.Previous = existingNode;
+            if (existingNode.Next != null) existingNode.Next.Previous = newNode;
+            existingNode.Next = newNode;
 
-    public void Remove(LinkedListNode<T> node)
-    {
-        if (node == null)
-        {
-            Console.WriteLine("Nodo nullo, ricontrollare");
-            return;
+            if (existingNode == _last) _last = newNode;
+            Count++;
         }
 
-        if (node.Previous != null) node.Previous.Next = node.Next;
-        else Head = node.Next;
+        public void AddAfter(LinkedListNode<T> existingNode, T value) 
+            => AddAfter(existingNode, new LinkedListNode<T>(value));
 
-        if (node.Next != null) node.Next.Previous = node.Previous;
-        else Last = node.Previous;
-        Count--;
-    }
-
-    public void Remove(T value)
-    {
-        LinkedListNode<T> node = Find(value);
-        if (node != null)
+        public void AddBefore(LinkedListNode<T> existingNode, LinkedListNode<T> newNode)
         {
-            Remove(node);
-            return;
-        }
-        Count--;
-    }
+            if (existingNode == null || newNode == null)
+            {
+                Console.WriteLine("Presenti nodi non esistenti nel comando, ricontrollare");
+                return;
+            }
 
-    public void RemoveFirst()
-    {
-        if (Head == null)
-        {
-            Console.WriteLine("lista vuota");
-            return;
+            newNode.Previous = existingNode.Previous;
+            newNode.Next = existingNode;
+            if (existingNode.Previous != null) existingNode.Previous.Next = newNode;
+            existingNode.Previous = newNode;
+
+            if (existingNode == _head) _head = newNode;
+            Count++;
         }
 
-        Remove(Head);
-    }
+        public void AddBefore(LinkedListNode<T> existingNode, T value) 
+            => AddBefore(existingNode, new LinkedListNode<T>(value));
 
-    public void RemoveLast()
-    {
-        if (Last == null)
+        public bool Contains(T value)
         {
-            Console.WriteLine("lista vuota");
-            return;
+            return Find(value) != null;
         }
-        Remove(Last);
-    }
 
-    public void Clear()
-    {
-        Head = Last = null;
-        Count = 0;
-    }
-
-    public override string ToString()
-    {
-        LinkedListNode<T> current = Head;
-        string result = "";
-        while (current != null)
+        public LinkedListNode<T> Find(T value)
         {
-            result += current.Value + " -> ";
-            current = current.Next;
+            LinkedListNode<T> current = _head;
+            while (current != null)
+            {
+                if (current.Value.Equals(value))
+                    return current;
+                current = current.Next;
+            }
+            return null;
         }
-        return result + "null";
+
+        public LinkedListNode<T> FindLast(T value)
+        {
+            LinkedListNode<T> current = _last;
+            while (current != null)
+            {
+                if (current.Value.Equals(value))
+                    return current;
+                current = current.Previous;
+            }
+            return null;
+        }
+
+        public void Remove(LinkedListNode<T> node)
+        {
+            if (node == null)
+            {
+                Console.WriteLine("Nodo nullo, ricontrollare");
+                return;
+            }
+
+            if (node.Previous != null) node.Previous.Next = node.Next;
+            else _head = node.Next;
+
+            if (node.Next != null) node.Next.Previous = node.Previous;
+            else _last = node.Previous;
+            Count--;
+        }
+
+        public void Remove(T value)
+        {
+            LinkedListNode<T> node = Find(value);
+            if (node != null)
+            {
+                Remove(node);
+                return;
+            }
+            Count--;
+        }
+
+        public void RemoveFirst()
+        {
+            if (_head == null)
+            {
+                Console.WriteLine("lista vuota");
+                return;
+            }
+
+            Remove(_head);
+        }
+
+        public void RemoveLast()
+        {
+            if (_last == null)
+            {
+                Console.WriteLine("lista vuota");
+                return;
+            }
+            Remove(_last);
+        }
+
+        public void Clear()
+        {
+            _head = _last = null;
+            Count = 0;
+        }
+
+        public override string ToString()
+        {
+            LinkedListNode<T> current = _head;
+            string result = "";
+            while (current != null)
+            {
+                result += current.Value + " -> ";
+                current = current.Next;
+            }
+            return result + "null";
+        }
     }
 }
